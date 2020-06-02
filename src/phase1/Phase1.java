2f60891be31165e60d5cbcabebbd2fe2a81bb5c7
Elena Piaggio <elenapiaggio@gmail.com>
2020-06-02 12:58:43 +0200

    ADD phase 1

---
+++ src/phase1/Phase1.java
@@ -0,0 +1,17 @@
+package phase1;
+
+public class Phase1 {
+  public static void main(String[] args) {
+    int five = 5;
+    int ten = 10;
+    int twenty = 20;
+    int fifty = 50;
+    int oneHundred = 100;
+    int twoHundred = 200;
+    int fiveHundred = 500;
+    int totalFood = 0;
+
+    String[] menuList = new String[5];
+    double[] foodPriceList = new double[5];
+  }
+}
